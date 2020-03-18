e67f902950801148a7221a826a3fd57b88e255ac
YYYYYYk <377073561@qq.com>
2020-03-18 15:29:16 +0800

    商品分类参数

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 //注意Message弹框提示组件（这个是全局挂载）
 import {Button,form,FormItem,input,Message,Container,Header,Aside,Main,Menu,MenuItem,Submenu,Breadcrumb,BreadcrumbItem,
-        Card,Row,Col,Table,TableColumn,Switch,Tooltip,Pagination,Dialog,MessageBox,Tag,Tree,Select,Option,Cascader} from 'element-ui'
+        Card,Row,Col,Table,TableColumn,Switch,Tooltip,Pagination,Dialog,MessageBox,Tag,Tree,Select,Option,Cascader,Alert,
+        Tabs,TabPane} from 'element-ui'
 
 Vue.use(Button)
 Vue.use(form)
@@ -30,6 +31,9 @@ Vue.use(Tree)
 Vue.use(Select)
 Vue.use(Option)
 Vue.use(Cascader)
+Vue.use(Alert)
+Vue.use(Tabs)
+Vue.use(TabPane)
 //这样就可通过this访问到$message
 Vue.prototype.$message=Message
 Vue.prototype.$confirm=MessageBox.confirm
